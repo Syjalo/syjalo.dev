67473943d82e6d4d44e357830b7ddbf8247eb5b5
Synbulat Biishev <syjalo.dev@gmail.com>
2023-04-29 21:43:03 +0300

    feat: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <main className='min-h-screen grid place-content-center text-6xl text-center font-bold text-gray-800'>
+      Welcome to Syjalo&apos;s website
+    </main>
+  )
+}
