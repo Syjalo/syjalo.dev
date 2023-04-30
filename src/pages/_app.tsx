141d972eb820310c60160bce3477d01e1344fe8d
Synbulat Biishev <syjalo.dev@gmail.com>
2023-04-30 15:18:57 +0300

    feat: add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Montserrat } from 'next/font/google';
+import Head from 'next/head';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={montserrat.className}>
+      <Head>
+        <title>Syjalo</title>
+      </Head>
       <Component {...pageProps} />
     </main>
   )
