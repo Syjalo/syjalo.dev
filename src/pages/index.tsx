ec2b930b19af4998b44db26daff19a8a0f70be19
Synbulat Biishev <syjalo.dev@gmail.com>
2023-04-30 15:07:49 +0300

    feat: update main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-export default function Welcome() {
+import { SiDiscord, SiGithub, SiMaildotru, SiTwitter, SiVk } from 'react-icons/si';
+
+
+export default function Main() {
   return (
-    <main className='min-h-screen grid place-content-center text-6xl text-center font-bold text-gray-800'>
-      Welcome to Syjalo&apos;s website
-    </main>
+    <div className='w-screen h-screen bg-neutral-900 text-neutral-200'>
+      <div className='w-full h-full mx-auto p-6 flex justify-center items-center'>
+        <div>
+          <h1 className='text-6xl font-bold'>Hello, I&apos;m Syjalo!</h1>
+          <p className='uppercase text-lg tracking-wides text-neutral-300'>Don&apos;t imagine, just do it</p>
+          <div className='flex items-center justify-between max-w-xs mx-auto my-4'>
+            <a href='mailto:contact@syjalo.dev' target='_blank' title='Email'>
+              <SiMaildotru className='h-8 w-8' />
+            </a>
+            <a href='https://github.com/Syjalo' target='_blank' title='GitHub'>
+              <SiGithub className='h-8 w-8' />
+            </a>
+            <div className='group' title='Discord'>
+              <span className='absolute w-auto p-2 min-w-max -translate-x-1/3 translate-y-10 rounded-md text-neutral-900 bg-neutral-300 text-xs font-bold transition-all duration-300 scale-0 group-hover:scale-100'>Syjalo#6046</span>
+              <SiDiscord className='h-8 w-8' />
+            </div>
+            <a href='https://vk.com/synbulat.biishev' target='_blank' title='VK'>
+              <SiVk className='h-8 w-8' />
+            </a>
+            <a href='https://twitter.com/Syjalo_' target='_blank' title='Twitter'>
+              <SiTwitter className='h-8 w-8' />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
